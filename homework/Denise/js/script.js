5b1a5a57997aecade3135774061fd887658538c5
Denise.L0pes <lopess.denise@gmail.com>
2020-06-28 17:37:07 -0300

    Criado alert para os casos em que as notas estiverem vazias e ajustados os textos das mensagens de aprovação e reprovação. Alterado título da página.

--- homework/Denise/js/script.js
+++ homework/Denise/js/script.js
@@ -2,23 +2,33 @@ const btnCalcMedia = document.querySelector('#button');
 const notaUm = document.querySelector('.nota1');
 const notaDois = document.querySelector('.nota2');
 const media = document.querySelector('.media');
+const mediaTitle = document.getElementsByClassName('media-titulo');
 
 function exibir(){
     const inputNotaUm = parseInt (notaUm.value);
+    console.log (inputNotaUm)
     const inputNotaDois = parseInt (notaDois.value);
+    console.log (inputNotaDois)
     const inputMedia = parseInt ((inputNotaUm+inputNotaDois)/2);
+    const notas = ('Suas notas foram '+inputNotaUm+ ' e '+inputNotaDois+ ', então sua média é');
     
-    if (inputMedia<7){
-        media.textContent = (' '+inputMedia+ '.' + '  Que pena! Você foi reprovado!');
+    if (isNaN(inputNotaUm)|| isNaN(inputNotaDois)){
+        alert (`Atenção! É necessário que você digite as duas notas.`);
+    }
+    else if (inputMedia<7){
+        alert (notas + ' '+ inputMedia + '.  Que pena! Você foi reprovado.');
+        media.textContent = (' '+inputMedia+ '.' + '  Poxa, reprovado.');
         media.style.color = 'red';
         notaUm.value ='';
         notaDois.value = '';
     }
     else{
-        media.textContent = (' '+inputMedia+ '.' + '  Que maravilha! Você foi aprovado!');
+        alert (notas + ' '+ inputMedia + '.  Maravilha! Você foi aprovado.');
+        media.textContent = (' '+inputMedia+ '.' + '  Ótimo! Aprovado ☺!');
         media.style.color = 'green';
+        notaUm.value ='';
+        notaDois.value = '';
     }
 }
-    
 btnCalcMedia.addEventListener("click", exibir)
   
