0ac9ba0669c98b08423cd074aa27f6ba495cfa24
Denise.L0pes <lopess.denise@gmail.com>
2020-07-02 18:12:08 -0300

    Acrescentado ao exercício um alerta interno (digamos assim) informando a média, notas e status da aprovação. Também foram sbstituidos os parseInt por number, bem como o parseInt da média por math.round.

--- homework/Denise/js/script.js
+++ homework/Denise/js/script.js
@@ -3,32 +3,50 @@ const notaUm = document.querySelector('.nota1');
 const notaDois = document.querySelector('.nota2');
 const media = document.querySelector('.media');
 const mediaTitle = document.getElementsByClassName('media-titulo');
+const popResult = document.querySelector('.resultado');
+const exibirNotas = document.querySelector('.notasShow');
+const exibirMedia = document.querySelector('.MediaShow');
+const exibirStatus = document.querySelector('.statusShow');
+const btnCiente = document.querySelector('#button-ciente');
 
-function exibir(){
-    const inputNotaUm = parseInt (notaUm.value);
+
+
+btnCalcMedia.addEventListener('click', function(){
+    const inputNotaUm = Number (notaUm.value);
     console.log (inputNotaUm)
-    const inputNotaDois = parseInt (notaDois.value);
+    const inputNotaDois = Number (notaDois.value);
     console.log (inputNotaDois)
-    const inputMedia = parseInt ((inputNotaUm+inputNotaDois)/2);
-    const notas = ('Suas notas foram '+inputNotaUm+ ' e '+inputNotaDois+ ', então sua média é');
-    
+    const inputMedia = Math.round ((inputNotaUm+inputNotaDois)/2);
+    const notas = (inputNotaUm+ ' e '+inputNotaDois);
+
     if (isNaN(inputNotaUm)|| isNaN(inputNotaDois)){
-        alert (`Atenção! É necessário que você digite as duas notas.`);
+        alert (`Atenção! É necessário que você digite as duas notas usando números de 0 a 10.`);
     }
+
     else if (inputMedia<7){
-        alert (notas + ' '+ inputMedia + '.  Que pena! Você foi reprovado.');
+        popResult.style.display = 'block'
+        exibirStatus.textContent = (' REPROVADO');
+        exibirStatus.style.color = 'red';
+        exibirNotas.textContent = (' '+notas);
+        exibirMedia.textContent = (' '+inputMedia);
         media.textContent = (' '+inputMedia+ '.' + '  Poxa, reprovado.');
         media.style.color = 'red';
-        notaUm.value ='';
-        notaDois.value = '';
     }
+
     else{
-        alert (notas + ' '+ inputMedia + '.  Maravilha! Você foi aprovado.');
-        media.textContent = (' '+inputMedia+ '.' + '  Ótimo! Aprovado ☺!');
+        popResult.style.display = 'block'
+        exibirStatus.textContent = (' APROVADO');
+        exibirStatus.style.color = 'green';
+        exibirNotas.textContent = (' '+notas);
+        exibirMedia.textContent = (' '+inputMedia);
+        media.textContent = (' '+inputMedia+ '.' + '  Maravilha, aprovado.');
         media.style.color = 'green';
-        notaUm.value ='';
-        notaDois.value = '';
     }
-}
-btnCalcMedia.addEventListener("click", exibir)
-  
+})
+
+btnCiente.addEventListener('click', function(){
+    popResult.style.display = 'none'
+    notaUm.value = '';
+    notaDois.value = '';
+    media.textContent = '';
+})
